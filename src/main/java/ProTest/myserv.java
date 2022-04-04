fa96d7d6c0c55ed07ccb5a40ff6510fb5fbcfa35
kiop1023 <jgim62393@gmail.com>
2022-04-04 11:25:37 +0900

    Changes

---
+++ src/main/java/ProTest/myserv.java
@@ -0,0 +1,21 @@
+package ProTest;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class myserv {
+    public static class MySocketClient {
+        @SuppressWarnings("resource")
+        public static void main(String[] args) {
+
+
+            try {
+                Socket socket = new Socket("192.168.0.72", 1234); // 소켓 서버에 접속
+                System.out.println("socket 서버에 접속 성공!");
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
