bd18ad15e5a91888d28991578eb13778aba2e5e4
kiop1023 <jgim62393@gmail.com>
2022-04-05 11:15:15 +0900

    Changes

--- src/main/java/serve.java
+++ src/main/java/serve.java
@@ -7,12 +7,12 @@ public class serve {
         try {
             int socketPort = 1234;
             ServerSocket serverSocket = new ServerSocket(socketPort); // 소켓 만들기
-            System.out.println("socket : " + socketPort + "으로 서버가 열렸습니다");
+            System.out.println("socket : " + socketPort + "으로 서버가 열렸슴니다");
 
             // 소켓 서버가 종료될 때까지 반복
             while(true) {
                 Socket socketUser = serverSocket.accept(); // 소켓 서버로 접속 시 socketUser에 접속자 정보 할당
-                System.out.println("Client가 접속함 : " + socketUser.getLocalAddress()); // 접속자의 getLocalAddress 가져오기
+                System.out.println("사용자가 접속함 : " + socketUser.getLocalAddress()); // 접속자의 getLocalAddress 가져오기
             }
 
         } catch (IOException e) {
